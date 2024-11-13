069febd72faf1bd9981beadf1a6706cc52ab7bc8
Teddy-Austin Tobenna <136184722+Tobennaaustin@users.noreply.github.com>
2024-11-13 22:43:28 +0100

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -7,8 +7,32 @@ const firebaseConfig = {
     appId: "1:278307611427:web:ddc00adfbb03b8eda5e296",
     measurementId: "G-H4SN9PCNBM"
 };
-
 export {firebaseConfig};
+
+// Import Firebase if you haven't already
+// import { initializeApp } from "firebase/app";
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+// Initialize Firebase (make sure to add your Firebase config here)
+// const firebaseConfig = { /* Your Firebase config here */ };
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+
+// Protect the main page
+document.addEventListener("DOMContentLoaded", () => {
+  const auth = firebase.auth();
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      // User is logged in, allow access
+      console.log("User is logged in:", user);
+    } else {
+      // No user is signed in, redirect to the login page
+      window.location.href = "login.html";
+    }
+  });
+});
+
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
